62c6a0b293a4ee17bcc1b4122f2311ad65e64819
CaioVMSantos <caiovictorcvm10@gmail.com>
2025-09-16 14:49:08 -0300

    feat: add create-table_event.sql on migration

---
+++ src/main/resources/db/migration/v1__create_table_event.sql
@@ -0,0 +1,13 @@
+--v1_create_table_event.sql
+CREATE TABLE Events(
+    id BIGSERIAL PRIMARY KEY ,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    location VARCHAR(255) NOT NULL,
+    identifier VARCHAR(255) NOT NULL UNIQUE,
+    start_time TIMESTAMP NOT NULL,
+    end_time TIMESTAMP NOT NULL,
+    capacity INTEGER NOT NULL,
+    type VARCHAR(50) NOT NULL,
+    organizer VARCHAR(255) NOT NULL
+)
