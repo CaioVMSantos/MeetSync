bfc2c103f080a16886579934c439846bc4230948
CaioVMSantos <caiovictorcvm10@gmail.com>
2025-09-16 14:54:00 -0300

    feat: Add EventEntity on infrastructure with @Entity

--- src/main/java/com/example/MeetSync/infrastructure/persistence/EventEntity.java
+++ src/main/java/com/example/MeetSync/infrastructure/persistence/EventEntity.java
@@ -1,4 +1,31 @@
 package com.example.MeetSync.infrastructure.persistence;
 
+import com.example.MeetSync.core.enums.EventType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name= "Events")
 public class EventEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private String location;
+    private String identifier;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private Long capacity;
+    @Enumerated(EnumType.STRING)
+    private EventType type;
+    private String organizer;
 }
