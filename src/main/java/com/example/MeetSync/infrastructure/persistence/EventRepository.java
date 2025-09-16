f4fc6f3031a1f5f4d53069f9d37e3ff1b363c33c
CaioVMSantos <caiovictorcvm10@gmail.com>
2025-09-16 18:25:34 -0300

    feat: Implements UseCases and Controller, Repository.

---
+++ src/main/java/com/example/MeetSync/infrastructure/persistence/EventRepository.java
@@ -0,0 +1,5 @@
+package com.example.MeetSync.infrastructure.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<EventEntity, Long> {}
